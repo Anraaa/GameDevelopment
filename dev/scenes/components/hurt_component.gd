6cd40ea415ae3806237fa7c4b50bb3021d6639cf
Anraaa <aqlaharun03@gmail.com>
2025-10-06 20:20:05 +0700

    membuat objek pohon agar bisa ditebang

--- dev/scenes/components/hurt_component.gd
+++ dev/scenes/components/hurt_component.gd
@@ -6,9 +6,7 @@ extends Area2D
 signal hurt
 
 func _on_area_entered(area: Area2D) -> void:
-	# Cek apakah Node yang masuk memang bertipe HitComponent
-	if area is HitComponent:
-		var hit_component = area as HitComponent # Casting aman karena sudah dicek
-		
-		if tool == hit_component.current_tool:
-			hurt.emit(hit_component.hit_damage)
+	var hit_component = area as HitComponent
+	
+	if tool == hit_component.current_tool:
+		hurt.emit(hit_component.hit_damage)
